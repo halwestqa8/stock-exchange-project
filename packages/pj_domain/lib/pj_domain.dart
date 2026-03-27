4dda06c8f4c941dd9008eca240ceb6b66de38053
halwestqa8 <halwestqa8@gmail.com>
2026-03-27 17:07:19 +0300

    Upload packages/pj_domain/lib/pj_domain.dart

---
+++ packages/pj_domain/lib/pj_domain.dart
@@ -0,0 +1,7 @@
+export 'src/models/user.dart';
+export 'src/models/category.dart';
+export 'src/models/vehicle_type.dart';
+export 'src/models/shipment.dart';
+export 'src/models/report.dart';
+export 'src/models/notification.dart';
+export 'src/models/pricing_config.dart';
