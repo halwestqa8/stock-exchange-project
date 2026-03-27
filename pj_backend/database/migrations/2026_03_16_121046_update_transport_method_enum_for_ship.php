ec2f051104ffa3b73bbd35f711e7fd58c2f2480b
halwestqa8 <halwestqa8@gmail.com>
2026-03-27 17:20:31 +0300

    Upload pj_backend/database/migrations/2026_03_16_121046_update_transport_method_enum_for_ship.php

---
+++ pj_backend/database/migrations/2026_03_16_121046_update_transport_method_enum_for_ship.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('vehicle_types', function (Blueprint $table) {
+            $table->dropColumn('transport_method');
+            $table->enum('transport_method', ['ground', 'air', 'sea'])->default('ground')->after('name_ku');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('vehicle_types', function (Blueprint $table) {
+            $table->dropColumn('transport_method');
+            $table->enum('transport_method', ['ground', 'air'])->default('ground')->after('name_ku');
+        });
+    }
+};
