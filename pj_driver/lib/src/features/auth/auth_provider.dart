e3effc15df190e0eed952f506d6339598082afce
halwestqa8 <halwestqa8@gmail.com>
2026-03-27 17:33:04 +0300

    Upload pj_driver/lib/src/features/auth/auth_provider.dart

---
+++ pj_driver/lib/src/features/auth/auth_provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pj_api_client/pj_api_client.dart';
+import 'package:pj_domain/pj_domain.dart';
+import '../../core/api_provider.dart';
+
+class AuthNotifier extends StateNotifier<User?> {
+  final ApiClient _apiClient;
+  AuthNotifier(this._apiClient) : super(null);
+
+  Future<void> login(String email, String password) async {
+    final response = await _apiClient.login(email, password);
+    _apiClient.setToken(response.data['access_token']);
+    state = User.fromJson(response.data['user']);
+  }
+
+  void logout() {
+    _apiClient.setToken(null);
+    state = null;
+  }
+}
+
+final authProvider = StateNotifierProvider<AuthNotifier, User?>((ref) {
+  return AuthNotifier(ref.watch(apiClientProvider));
+});
