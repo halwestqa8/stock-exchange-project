f8f4fdb63c1a442dd270bbc622db4c36b1ddb102
halwestqa8 <halwestqa8@gmail.com>
2026-03-27 17:19:34 +0300

    Upload pj_backend/app/Models/Notification.php

---
+++ pj_backend/app/Models/Notification.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Notification extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'message_en',
+        'message_ku',
+        'type',
+        'is_read',
+        'image_url',
+        'location',
+    ];
+
+    protected $casts = [
+        'is_read' => 'boolean',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
