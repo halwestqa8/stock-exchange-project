e2c7245be32a24e273c9ebb1419a99b200d968e0
halwestqa8 <halwestqa8@gmail.com>
2026-03-27 17:23:56 +0300

    Upload pj_customer/lib/main.dart

---
+++ pj_customer/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'src/core/router.dart';
+import 'src/core/theme.dart';
+
+import 'package:pj_shared_ui/pj_shared_ui.dart';
+import 'package:pj_l10n/pj_l10n.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  runApp(
+    ProviderScope(
+      overrides: [sharedPreferencesProvider.overrideWithValue(prefs)],
+      child: const LTMSCustomerApp(),
+    ),
+  );
+}
+
+class LTMSCustomerApp extends ConsumerWidget {
+  const LTMSCustomerApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+    final locale = ref.watch(localeProvider);
+
+    return MaterialApp.router(
+      key: ValueKey('app_${locale.languageCode}'),
+      onGenerateTitle: (context) => L10n.of(context)!.ltmsCustomer,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.light,
+      routerConfig: router,
+      locale: locale,
+      supportedLocales: L10n.supportedLocales,
+      localizationsDelegates: [
+        L10n.delegate,
+        if (locale.languageCode == 'ku') ...[
+          KurdishMaterialLocalizations.delegate,
+          KurdishCupertinoLocalizations.delegate,
+        ],
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+    );
+  }
+}
