f7f0e9058429cf41f2229fdc531f5e0d543c4e10
halwestqa8 <halwestqa8@gmail.com>
2026-03-27 17:48:27 +0300

    Upload pj_admin/lib/src/features/categories/category_management_screen.dart

---
+++ pj_admin/lib/src/features/categories/category_management_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pj_l10n/pj_l10n.dart';
+import '../../core/dashboard_back_button.dart';
+
+class CategoryManagementScreen extends ConsumerWidget {
+  const CategoryManagementScreen({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const DashboardBackButton(),
+        title: Text(L10n.of(context)!.categories),
+      ),
+      body: Center(child: Text(L10n.of(context)!.categoryCrudPlaceholder)),
+    );
+  }
+}
